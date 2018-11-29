db07878215459a5dbf44001e865f8cb3af031e3d
venkat09 <venkat09docs@gmail.com>
2018-11-29 11:30:16 +0530

    Create variables.tf

---
+++ ec2-instance/variables.tf
@@ -0,0 +1,21 @@
+variable "aws_access_key" {
+  default = ""
+}
+
+variable "aws_secret_key" {
+  default = ""
+}
+
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "vpc_id" {
+  default = "vpc-3d5a045a"
+}
+variable "subnet_id" {
+default = "subnet-80a736e5"
+}
+variable "name" {
+default = "rns"
+}
