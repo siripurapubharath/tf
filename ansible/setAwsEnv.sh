1326729e6c4e5f249b3d2b171c0e5b4df406f418
venkat09 <venkat09docs@gmail.com>
2018-12-01 02:04:37 +0530

    Update setAwsEnv.sh

--- ansible/setAwsEnv.sh
+++ ansible/setAwsEnv.sh
@@ -1,4 +1,5 @@
-yum install ansible python-boto -y
+sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+#yum install ansible python-boto -y
 export AWS_SECRET_ACCESS_KEY=''
 export AWS_ACCESS_KEY_ID=''
 export ANSIBLE_HOSTS=~/ansible/ec2.py
