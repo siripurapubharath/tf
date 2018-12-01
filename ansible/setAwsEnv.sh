8a47670be31f23e9a2d07d305a5dfb1027a93ce8
venkat09 <venkat09docs@gmail.com>
2018-12-01 20:36:52 +0530

    Update setAwsEnv.sh

--- ansible/setAwsEnv.sh
+++ ansible/setAwsEnv.sh
@@ -2,7 +2,7 @@ sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -
 #yum install ansible python-boto -y
 export AWS_SECRET_ACCESS_KEY=''
 export AWS_ACCESS_KEY_ID=''
-export ANSIBLE_HOSTS=~/ansible/ec2.py
-export EC2_INI_PATH=~/ansible/ec2.ini
+export ANSIBLE_HOSTS=$PWD/ec2.py
+export EC2_INI_PATH=$PWD/ec2.ini
 ssh-agent bash
 ssh-add ~/ansible/msrKey.pem
