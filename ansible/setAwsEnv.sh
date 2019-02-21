b1eb4d4e6a5a89c93999c61b5f91702d97c1f65e
root <root@localhost.localdomain>
2019-02-21 19:39:46 +0000

    cleared

--- ansible/setAwsEnv.sh
+++
@@ -1,8 +0,0 @@
-sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
-#yum install ansible python-boto -y
-export AWS_SECRET_ACCESS_KEY=''
-export AWS_ACCESS_KEY_ID=''
-export ANSIBLE_HOSTS=$PWD/ec2.py
-export EC2_INI_PATH=$PWD/ec2.ini
-ssh-agent bash
-ssh-add ~/ansible/msrKey.pem
