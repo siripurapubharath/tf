a63ea97fa155a92055f3e5da15881169acb67684
venkat09 <venkat09docs@gmail.com>
2019-02-22 01:14:43 +0530

    Update variables-secrets.tf

--- variables-secrets.tf
+++ variables-secrets.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
  description = "The AWS access key."
- default     = "AKIAJ5YS46ICPFGT72RQ"
+ default     = ""
 }
 
 variable "aws_secret_key" {
  description = "The AWS secret key."
- default     = "GbhRPW+EtgQj3WxOeu+fwnqj7jyzmx/PsqVzJ3Lj"
+ default     = ""
 }
